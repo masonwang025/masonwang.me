6413d5956cf0aa3df0ff2a2ad9c6f45bb377fcd7
MasonWang025 <mason.wang0025@gmail.com>
2021-12-27 22:32:08 -1000

    color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,38 +8,20 @@ module.exports = {
     extend: {
       colors: {
         mw: {
-          light: {
-            light: "#fcf5ff",
-            primary: "#F9EDFF",
-            dark: "##e9d5f2",
+          bg: {
+            primary: "#E3E2Df",
+            darkest: "#E3AFBC",
           },
-          purple: {
-            light: "#b691d9",
-            primary: "#A880CE",
-            dark: "#8d66b3",
-          },
-          green: {
-            light: "#8cd4a7",
-            primary: "#7BC697",
-            dark: "#63ab7e",
-          },
-          blue: {
-            light: "#8bcfe0",
-            primary: "#74C0D3",
-            dark: "#61aec2",
-            darker: "#57a2b5",
+          accent: {
+            primary: "#E40044",
+            darker: "#bd0038",
           },
           dark: {
-            light: "##0c3057",
-            primary: "#05203D",
-            dark: "##02101f",
+            primary: "#5D001E",
           },
         },
       },
     },
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-  ],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
